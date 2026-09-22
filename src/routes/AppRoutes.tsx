73e82ef37d878a1ba24edfcb8dde5d3c3d742905
xfelixp47 <xfelixp47@gmail.com>
2026-09-22 17:23:18 -0400

    Cambios del proyecto

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import LoginForm from "../components/auth/LoginForm";
-
+import LoginPage from "../pages/auth/LoginPage";
 import HomePage from "../pages/HomePage";
 import InventoryPage from "../pages/InventoryPage";
 import LoansPage from "../pages/LoansPage";
@@ -15,49 +14,25 @@ import NewLoanPage from "../pages/NewLoanPage";
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginForm />} />
+      <Route path="/login" element={<LoginPage />} />
 
       <Route path="/" element={<HomePage />} />
 
-      <Route
-        path="/inventario"
-        element={<InventoryPage />}
-      />
+      <Route path="/inventario" element={<InventoryPage />} />
 
-      <Route
-        path="/inventario/nuevo"
-        element={<NewObjectPage />}
-      />
+      <Route path="/prestamos" element={<LoansPage />} />
 
-      <Route
-        path="/prestamos"
-        element={<LoansPage />}
-      />
+      <Route path="/categorias" element={<CategoriesPage />} />
 
-      <Route
-        path="/prestamos/nuevo"
-        element={<NewLoanPage />}
-      />
+      <Route path="/reportes" element={<ReportsPage />} />
 
-      <Route
-        path="/categorias"
-        element={<CategoriesPage />}
-      />
+      <Route path="/usuarios" element={<UsersPage />} />
 
-      <Route
-        path="/reportes"
-        element={<ReportsPage />}
-      />
+      <Route path="/configuracion" element={<SettingsPage />} />
 
-      <Route
-        path="/usuarios"
-        element={<UsersPage />}
-      />
+      <Route path="/nuevo-objeto" element={<NewObjectPage />} />
 
-      <Route
-        path="/configuracion"
-        element={<SettingsPage />}
-      />
+      <Route path="/nuevo-prestamo" element={<NewLoanPage />} />
     </Routes>
   );
 }
